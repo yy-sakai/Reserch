d7cb472f2b2261fc820e4ea8cb06a66cb9a8ce1f
yy-sakai <92081039+yy-sakai@users.noreply.github.com>
2023-11-26 12:28:07 +0900

    Creation of images for each tau.

--- Codes/baf_for_wassserstein/jko_pme/euler_explicit_m=2img.py
+++ Codes/baf_for_wassserstein/jko_pme/euler_explicit_m=2img.py
@@ -1,15 +1,12 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-image_root = "../images/euler_tau"
+image_root = "../images/euler_tau/"
 
-img_tau000625 = np.load(f'{image_root}tau=0.00625.npy')
-img_tau00125 = np.load(f'{image_root}tau=0.0125.npy')
-img_tau0025 = np.load(f'{image_root}tau=0.025.npy')
-img_tau005 = np.load(f'{image_root}tau=0.05.npy')
-img_tau01 = np.load(f'{image_root}tau=0.1.npy')
-img_tau02 = np.load(f'{image_root}tau=0.2.npy')
-img_tau04 = np.load(f'{image_root}tau=0.4.npy')
+img_tau001 = np.load(f'{image_root}tau=0.001.npy')
+img_tau0001 = np.load(f'{image_root}tau=0.0001.npy')
+img_tau00005 = np.load(f'{image_root}tau=5e-05.npy')
+img_tau000025 = np.load(f'{image_root}tau=2.5e-05.npy')
 img_exact = np.load(f'{image_root}exact.npy')
 
 x = np.linspace(-0.5, 0.5, 513)
@@ -20,13 +17,10 @@ plt.ylim([-0.1, 15.1])
 for i in range(4):
     plt.ylim([-0.1, 15.1])
     plt.plot(x, img_exact[i], "--", label=r'$exact$')
-    plt.plot(x, img_tau04[i],label=r'$\tau = 0.4$')
-    plt.plot(x, img_tau02[i],label=r'$\tau = 0.2$')
-    plt.plot(x, img_tau01[i],label=r'$\tau = 0.1$')
-    plt.plot(x, img_tau005[i],label=r'$\tau = 0.05$')
-    plt.plot(x, img_tau0025[i],label=r'$\tau = 0.025$')
-    plt.plot(x, img_tau00125[i],label=r'$\tau = 0.0125$')
-    plt.plot(x, img_tau000625[i],label=r'$\tau = 0.00625$')
+    plt.plot(x, img_tau001[i],label=r'$\tau = 0.001$')
+    plt.plot(x, img_tau0001[i],label=r'$\tau = 0.0001$')
+    plt.plot(x, img_tau00005[i],label=r'$\tau = 0.00005$')
+    plt.plot(x, img_tau000025[i],label=r'$\tau = 0.000025$')
     plt.legend()
     plt.savefig(f'{image_root}t={t[i]}.png')
     plt.close()
